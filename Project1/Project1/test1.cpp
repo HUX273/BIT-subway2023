ec247aba77dc649ea54d94bd4c207d44c1e7f0ae
hux <1321992397@qq.com>
2023-05-21 15:02:04 +0800

    update func3(unfinished)

--- Project1/Project1/test1.cpp
+++ Project1/Project1/test1.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <map>
 #include <queue>
+#include<ctime>
+
 using namespace std;
 
 const int INF = 0x7f7f7f7f;
@@ -14,10 +16,11 @@ const int INF = 0x7f7f7f7f;
 const int MAX_POINT = 500; //车站的个数上限
 const int MAX_LINE = 50; //路线的个数上限
 
+
 string line_name[MAX_LINE]; // 路线的名字
 vector<int> line[MAX_LINE]; // 路线中有哪些车站
 map<string, int> point_index;	//每个站的标号
-string point_name[MAX_POINT]; // 每个站的名字
+string point_name[MAX_POINT];	// 每个站的名字
 vector<int> neighbor[MAX_POINT];	// 点a的第i个邻居的站点序号为：neighbor[a][i]				//
 vector<int> cost[MAX_POINT];        // 点a到第i个邻居的距离为：cost[a][i]						// 邻居的顺序是由输入文件决定的
 vector<int> from_line[MAX_POINT];   // 点a到第i个邻居的地铁路线序号为：from_line[a][i]			//
@@ -27,32 +30,31 @@ int pre[MAX_POINT];     //起点到a点的最短路径上，a点之前的一个点是pre[a]
                         //因此到a点的最短路径为 start_point -> ... -> pre[pre[a]] -> pre[a] -> a
 int pre_line[MAX_POINT];//a到pre[a]的路线属于pre_line路线
 
+bool vis_func3[MAX_POINT];	//功能3专用标记
+int remaining_point = -1;	//功能三剩余点数
+
+
 //线路数
 int line_count;
 //车站数
 int point_count;
+//标识是哪个fun调用此模块
+int fun = 0;
 
+//根据站点名字返回 标号
 inline int to_point_index(string name) {
 	return point_index[name];
 }
+//根据标号返回 站点名字
 inline string to_point_name(int index) {
 	return point_name[index];
-}
-
-//更新站点的邻接关系
-void add_edge(int x, int y, int l, int c = 1) {
-	neighbor[x].push_back(y);	//点y是点x的邻居
-	cost[x].push_back(c);		//目前不考虑换线开销，故所有点连线的cost都默认为1
-	from_line[x].push_back(l);	//x到y的路线是在哪条地铁线上
-	neighbor[y].push_back(x);	//点x是点y的邻居
-	cost[y].push_back(c);		
-	from_line[y].push_back(l);	//y到x的路线是在哪条地铁线上（当然与上面一致）
 }
 
 void init()
 {
 	memset(dis, INF, sizeof(dis));
 	memset(vis, false, sizeof(vis));
+	memset(vis_func3, false, sizeof(vis_func3));
 	line_count = point_count = 0;
 }
 
@@ -147,7 +149,74 @@ string line_list() {
 	return res + "\n";
 }
 
-//查询并输出路线上的站点
+//更新站点的邻接关系
+void add_edge(int x, int y, int l, int c = 1) {
+	neighbor[x].push_back(y);	//点y是点x的邻居
+	cost[x].push_back(c);		//目前不考虑换线开销，故所有点连线的cost都默认为1
+	from_line[x].push_back(l);	//x到y的路线是在哪条地铁线上
+	neighbor[y].push_back(x);	//点x是点y的邻居
+	cost[y].push_back(c);		
+	from_line[y].push_back(l);	//y到x的路线是在哪条地铁线上（当然与上面一致）
+}
+
+//初始化所有站点的邻接关系、站点连线所在的地铁线路（不考虑换乘开销）
+void build_graph_normal() {
+	for (int i = 0; i < line_count; ++i) {//沿着不同的地铁线路来初始化在地铁线上的各个站点的邻接关系，并记录站点连线是几号线（即i）
+		int pre_point = -1;//有效站点的index值从0开始
+		for (int point : line[i]) {//按line[i]内站点的顺序，初始化i这条地铁线上各站点的信息
+			if (pre_point >= 0) {//如果 pre_point 是有效站点
+				add_edge(pre_point, point, i);//初始化
+			}
+			pre_point = point;//把这次循环初始化的站点置为前站点
+		}
+	}
+}
+
+//计算路过站点数
+int count_step(int t, int s) {
+	int temp = t;
+	int count = 1;
+	while (temp != s) {
+		count++;
+		temp = pre[temp];
+		//func3标记更新
+		if (vis_func3[temp] == false) {
+			vis_func3[temp] = true;
+			remaining_point--;
+		}
+	}
+
+	return count;
+}
+
+
+//生成输出路线
+string route(int s, int t) {
+	string res = "";
+	int temp = t;
+	int last_line = -1;
+	do {
+		if (temp != t) {
+			res += to_point_name(t);//string上加站点名称
+		}
+		if (fun == 2) {
+			if (pre_line[t] != last_line && last_line != -1) {//如果和上次坐的线不同了就要输出换乘信息
+				res += " 换乘" + line_name[pre_line[t]];
+			}
+		}
+		
+		last_line = pre_line[t];
+		if (temp != t) {
+			res += "\n";
+		}
+		t = pre[t];
+	} while (t != s);
+	res += to_point_name(t);
+
+	return res + "\n";
+}
+
+//功能一：查询并输出路线上的站点
 int task_line_req() {
 	while (true) {
 		cout << "输入0退出，输入?显示所有路线。" << endl;
@@ -166,53 +235,47 @@ int task_line_req() {
 	return 0;
 }
 
-//初始化所有站点的邻接关系、站点连线所在的地铁线路（换乘开销暂不考虑）
-void build_graph_normal() {
-	for (int i = 0; i < line_count; ++i) {//沿着不同的地铁线路来初始化在地铁线上的各个站点的邻接关系，并记录站点连线是几号线（即i）
-		int pre_point = -1;//有效站点的index值从0开始
-		for (int point : line[i]) {//按line[i]内站点的顺序，初始化i这条地铁线上各站点的信息
-			if (pre_point >= 0) {//如果 pre_point 是有效站点
-				add_edge(pre_point, point, i);//初始化
-			}
-			pre_point = point;//把这次循环初始化的站点置为前站点
-		}
-	}
-}
-
-//生成输出路线和途径站点数
-string route(int s, int t) {
-	string res = "\n";
-
-	int temp_t = t;
-	int count_point = 1;
-	while (temp_t != s) {
-		count_point ++;
-		temp_t = pre[temp_t];
-	}
-	res += to_string(count_point);
-	res += "\n";
-
-	int last_line = -1;
-	do {
-		res += to_point_name(t);//string上加站点名称
-		if (pre_line[t] != last_line && last_line != -1) {//如果和上次坐的线不同了就要输出换乘信息
-			res += " 换乘" + line_name[pre_line[t]];
-		}
-		last_line = pre_line[t];
-		res += "\n";
-		t = pre[t];
-	} while (t != s);
-	res += to_point_name(t) + "\n";
-	return res + "\n";
-}
-
-// 计算两个车站之间的最短距离（换乘不额外计距离）。返回需要输出的字符串。因为相邻车站之间的距离都为1，使用BFS算法。 
-string dis_normal(string src_name, string tar_name) {
-	int t = to_point_index(src_name), s = to_point_index(tar_name);
+//功能二：计算两个车站之间的最短距离（换乘不额外计距离）。返回需要输出的字符串。因为相邻车站之间的距离都为1，使用BFS算法。 
+string display_normal(string src_name, string tar_name) {
+	fun = 2;
+	int t = to_point_index(src_name), s = to_point_index(tar_name);//！！！t是起点，s是终点（与后续程序命名冲突，后续修改）
 	if (s == t) {//如果起点就是终点
 		return "\n" + (string)"1" + "\n" + src_name + "\n\n";
 	}
-	build_graph_normal();
+	build_graph_normal();//建图
+	queue<int> q;//先进先出
+	q.push(s);//终点站序号入队
+	dis[s] = 0;//把自己到自己的距离更新为0
+	vis[s] = true;//标记此点，说明已经走过
+	while (!q.empty()) {
+		int point = q.front();//取队列第一个数的值
+		q.pop();//删除队列第一个数
+		for (int i = 0; i < neighbor[point].size(); ++i) {
+			int nex_point = neighbor[point][i];
+			if (!vis[nex_point]) {//没走过该点
+				vis[nex_point] = 1;//标记此点
+				dis[nex_point] = dis[point] + 1;
+				pre[nex_point] = point;
+				pre_line[nex_point] = from_line[point][i];
+				if (nex_point == t) {
+					break;//到达终点break
+				}
+				q.push(nex_point);
+			}
+		}
+		if (vis[t]) break;
+	}
+
+	string res= "\n";
+	int count_point = count_step(t, s);
+	res = res + to_string(count_point) + "\n" + src_name + route(s, t);
+	return res;
+
+}
+
+
+string bfs_func3(int point_now, int next_point) {
+	int t = point_now, s = next_point;
 	queue<int> q;//先进先出
 	q.push(s);//终点站序号入队
 	dis[s] = 0;//把自己到自己的距离更新为0
@@ -238,36 +301,140 @@ string dis_normal(string src_name, string tar_name) {
 	return route(s, t);
 }
 
+//功能三：计算尽可能快地遍历地铁的所有车站的路线
+string display_func3(string src_name) {
+	fun = 3;
+	string res = src_name + "\n";
+	int step = 1;	//记录经过站点数
+	int st = to_point_index(src_name);//起点
+	int point_now = st;//此时所在点
+	int next_point = -1;
+	remaining_point = point_count;
+	build_graph_normal();//建图
+	
+	remaining_point--;		//
+	vis_func3[st] = true;	//排除起点
+							//
+
+	while (remaining_point != 0) {	// point_now 的 neighbor 中随机选一个 vis_func3 != true 的点作为下一点
+		int neighbor_size = neighbor[point_now].size();
+		int *point_unreached = new int[neighbor_size];
+		int j = 0;	//point_unreached的下标
+		for (int i = 0; i < neighbor_size; i++) {
+			if (vis_func3[neighbor[point_now][i]] != true) {
+				point_unreached[j] = neighbor[point_now][i];
+				j++;
+			}
+		}
+
+		if (j != 0) {	//j != 0 说明point_unreached不为空
+			//取随机数
+			srand((unsigned int)time(NULL));
+			int temp = rand() % j;
+			next_point = point_unreached[temp];
+
+			res += to_point_name(next_point) + "\n";
+
+			vis_func3[next_point] = true;
+			point_now = next_point;
+			remaining_point--;
+			step++;
+
+			delete point_unreached;
+		} else {		//point_unreached为空，说明邻居都被标记了，从全局找 next_point
+			int *point_unreached_2 = new int[point_count];
+			int k = 0;//point_unreached_2的下标
+			for (int i = 0; i < point_count; i++) {
+				if (vis_func3[i] != true) {
+					point_unreached_2[k] = i;
+					k++;
+				}
+			}
+
+			srand((unsigned int)time(NULL));
+			int temp = rand() % k;
+			next_point = point_unreached_2[temp];
+
+
+			//调用bfs
+			bfs_func3(point_now, next_point);
+			res += bfs_func3(point_now, next_point);
+
+
+			step += (count_step(point_now,next_point) - 1);
+			remaining_point;
+			vis_func3[next_point] = true;
+			point_now = next_point;
+
+			delete point_unreached_2;
+
+			//初始化所有func2中使用的所有全局变量
+			memset(vis, false, sizeof(vis));
+			memset(dis, INF, sizeof(dis));
+			memset(pre, 0, sizeof(pre));
+			memset(pre_line, 0, sizeof(pre_line));
+		}
+	}
+
+	//所有点走完了，回起点
+	if (remaining_point == 0) {	
+
+		next_point = st;
+		//调用bfs
+		bfs_func3(point_now, next_point);
+		res += bfs_func3(point_now, next_point);
+
+		step += (count_step(point_now, next_point) - 1);
+		remaining_point;
+		vis_func3[next_point] = true;
+		point_now = next_point;
+	}
+
+
+
+	return to_string(step) + "\n" + res;
+}
+
+
+
+
 int main(int argc, char *argv[])
 {
 	//调试功能二：
 	argc = 4;
-	argv[1] = const_cast<char*>("/b");
-	argv[2] = const_cast<char*>("一号点");
-	argv[3] = const_cast<char*>("七号点");
-
+	argv[1] = const_cast<char*>("/a");
+	argv[2] = const_cast<char*>("十号点");
+	argv[3] = const_cast<char*>("十二号点");
 
 
 	//输入 n m,初始化各点距离，相连的点距离为1，未相连的点距离为INF,
 	init();
 	input_map();
 
+	// 没有额外参数，进入功能一：查询路线，输出路线上所有站点
 	if (argc == 1) {
-		// 没有额外参数，进入功能一：查询路线，输出路线上所有站点
+		
 		return task_line_req();
 	}
 
+	//命令行调用程序加上了参数 /b ，进入功能二：计算所输入两个车站间的最短路线
 	if (strcmp(argv[1], "/b") == 0) {
-		// 命令行调用程序加上了参数 /b ，进入功能二：计算所输入两个车站间的最短路线
-
-		// 换乘没有额外距离的情况
-		cout << dis_normal((string)argv[2], (string)argv[3]);
 		
-		// 换乘距离记为3（待完成）
-		//以1为起点跑一次dij
-		//dijkstra(1);
+		// 换乘没有额外距离的情况
+		cout << display_normal((string)argv[2], (string)argv[3]);
+		
+		// 换乘开销记为3（待完成）
+
 	}
 
+	//命令行调用程序加上了参数 /a ，进入功能三：计算尽可能快地遍历地铁的所有车站的路线(先不考虑换乘开销，地图为等权无向图）
+	if (strcmp(argv[1], "/a") == 0) {
+		
+		// 换乘没有额外距离的情况
+		cout << display_func3((string)argv[2]);
+
+
+	}
 	
 	
 	return 0;
